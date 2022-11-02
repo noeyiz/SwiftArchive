e4eda3cbcb82b271bc68fd8bf0286c6f808216d3
noeyiz <lamoureclatant@gmail.com>
2022-11-02 17:57:31 +0900

    test: App Life Cycle

---
+++ AppLifeCycleTest/AppLifeCycleTest/SceneDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  SceneDelegate.swift
+//  AppLifeCycleTest
+//
+//  Created by jiyeon on 2022/11/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    var stickerView = UIView(frame: UIScreen.main.bounds)
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        stickerView.backgroundColor = .systemPink
+        print("scene")
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        
+        print("scene: DidDisconnect")
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        stickerView.removeFromSuperview()
+        print("scene: DidBecomeActive")
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        
+        window?.addSubview(stickerView)
+        print("scene: WillResignActive")
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        
+        print("scene: WillEnterForeground")
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        
+        print("scene: DidEnterBackground")
+    }
+
+
+}
+
